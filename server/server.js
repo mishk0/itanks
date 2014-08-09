2d4da4cb2729a186f3181559caeeb80802669f37
bach <bacherko@gmail.com>
2014-08-09 20:35:03 +0400

    Server modification

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ server.listen(port, function() {
     console.log('SERVER IS STARTED on port:', port);
 });
 
-var SOCKET_SEND_INTERVAL = 2000;
+var SOCKET_SEND_INTERVAL = 60;
 var GAME_LOGIN_UPDATE_INTERVAL = 5;
 
 var MAP_STATE = {
@@ -23,29 +23,48 @@ var MAP_STATE = {
     INDES: 2
 };
 
-var TANK_SPEED = 400;
-var BULLET_SPEED = 1000;
+var FIELD_DIMENSION = 26;
+
+var TANK_SPEED = 0.2;
+var BULLET_SPEED = 0.4;
 var GUN_RECOIL = 1000;
 
 var PLAYERS = [];
 var BULLETS = [];
 
 var DEFAULT_PLAYER = {
+    id: null,
     hp: 1,
     direction: 0,
     position: [0, 0],
-    inMove: true,
+    inMove: false,
     lastShootTS: 0,
     recoilTime: GUN_RECOIL,
     bulletSpeed: BULLET_SPEED,
     speed: TANK_SPEED,
-    socket: null
+    socket: null,
+    joint: false
 };
+
+DEFAULT_BOT = _.extend({}, DEFAULT_PLAYER, {
+    color: 'red',
+    inMove: true,
+    joint: true
+});
+
+for (var c = 0; c < 5; ++c) {
+    PLAYERS.push(_.extend({}, DEFAULT_BOT, {
+        id: String(Math.random()).substr(2),
+        name: 'bot',
+        direction: Math.floor(Math.random() * 4),
+        position: generateRandomIntegerPosition()
+    }));
+}
 
 var MAP = require('../maps/map1.js');
 
-for (var i = 0; i < 26; ++i) {
-    for (var j = 0; j < 26; ++j) {
+for (var i = 0; i < FIELD_DIMENSION; ++i) {
+    for (var j = 0; j < FIELD_DIMENSION; ++j) {
         var cell = MAP[i][j];
 
         if (cell === MAP_STATE.DESTR) {
@@ -64,8 +83,10 @@ wsServer.on('request', function(request) {
 
         var connection = request.accept(null, request.origin);
 
-        var player = _.clone(DEFAULT_PLAYER);
-        player.socket = connection;
+        var player = _.extend({}, DEFAULT_PLAYER, {
+            position: generateRandomIntegerPosition(),
+            socket: connection
+        });
 
         PLAYERS.push(player);
 
@@ -77,36 +98,70 @@ wsServer.on('request', function(request) {
         }, player);
 
         connection.on('message', function(message) {
-            if (message.type === 'utf8') {
-                var data;
+            if (message.type !== 'utf8') {
+                return;
+            }
 
-                try {
-                    data = JSON.parse(message.utf8Data);
-                } catch(e) {
-                    console.log('MESSAGE BAD JSON', message.utf8Data);
-                    return;
-                }
+            var messageData;
 
-                switch (data.action) {
-                    case 'updatestate':
-                        player.direction = data.direction;
-                        player.inMove = data.isMove;
-                        break;
+            try {
+                messageData = JSON.parse(message.utf8Data);
+            } catch(e) {
+                console.log('MESSAGE BAD JSON', message.utf8Data);
+                return;
+            }
 
-                    case 'shoot':
-                        var now = new Date().getTime();
+            var data = messageData.data;
 
-                        if (player.lastShootTS + player.recoilTime > now) {
-                            player.lastShootTS = now;
+            switch (messageData.action) {
+                case 'login':
+                    player.id = String(Math.random()).substr(2);
+                    player.color = 'red';
+                    player.joint = true;
+                    player.name = data.name;
+                    player.tankType = data.tankType;
 
-                            BULLETS.push({
-                                position: player.position,
-                                direction: player.direction,
-                                speed: player.bulletSpeed
-                            });
-                        }
-                        break;
-                }
+                    send(player, {
+                        event: 'playerList',
+                        data: PLAYERS.filter(jointFilter).map(function(player) {
+                            return {
+                                id: player.id,
+                                name: player.name,
+                                color: player.color,
+                                kills: 0,
+                                deaths: 0
+                            };
+                        })
+                    });
+
+                    send(player, {
+                        event: 'map',
+                        data: MAP
+                    });
+
+                    break;
+
+                case 'updateState':
+                    player.direction = data.direction;
+                    player.inMove = data.inMove;
+                    break;
+
+                case 'shoot':
+                    var now = new Date().getTime();
+
+                    if (player.lastShootTS + player.recoilTime > now) {
+                        player.lastShootTS = now;
+
+                        BULLETS.push({
+                            position: player.position,
+                            direction: player.direction,
+                            speed: player.bulletSpeed
+                        });
+                    }
+                    break;
+
+                default:
+                    console.log('UNSUPPORTED INCOMMING MESSAGE', messageData.action, data);
             }
         });
 
@@ -125,54 +180,123 @@ wsServer.on('request', function(request) {
     }
 });
 
+function generateRandomIntegerPosition() {
+    return [Math.floor(Math.random() * FIELD_DIMENSION), Math.floor(Math.random() * FIELD_DIMENSION)];
+}
+
 /*
  * World Logic update.
  */
 setInterval(function() {
     PLAYERS.forEach(function(player) {
-        if (player.inMove) {
-            player.position = updatePosition(player);
+        if (player.joint && player.inMove) {
+            currentPosition = player.position;
+
+            updatePosition(player);
+
+            if (!checkCollision(player)) {
+                switch (player.direction) {
+                    case 0:
+                        player.position[0] = Math.ceil(currentPosition[0] + 0.9) - 0.9;
+                        break;
+
+                    case 1:
+                        player.position[1] = Math.ceil(currentPosition[1] + 0.9) - 0.9;
+                        break;
+
+                    case 2:
+                        player.position[0] = Math.ceil(currentPosition[0] - 0.9) + 0.9;
+                        break;
+
+                    case 3:
+                        player.position[1] = Math.ceil(currentPosition[1] - 0.9) + 0.9;
+                        break;
+                }
+
+            }
         }
     });
 
     BULLETS.forEach(function(bullet) {
-        bullet.position = updatePosition(bullet);
+        updatePosition(bullet);
     });
 
 }, GAME_LOGIN_UPDATE_INTERVAL);
 
 
-function updatePosition(object) {
-    var curPos = object.position;
-    var toPos = _.clone(curPos);
+/**
+ * Ищет коллизии со стенами.
+ * @param {Object} obj
+ * @return {boolean}
+ */
+function checkCollision(obj) {
+    var posX = obj.position[0];
+    var posY = obj.position[1];
 
+    var posX1 = posX - 0.9;
+    var posX2 = posX + 0.9;
+
+    var posY1 = posY - 0.9;
+    var posY2 = posY + 0.9;
+
+    var x1 = Math.floor(posX1);
+    var x2 = Math.ceil(posX2);
+
+    var y1 = Math.floor(posY1);
+    var y2 = Math.ceil(posY2);
+
+    if (x1 < 0 || x2 >= FIELD_DIMENSION ||
+        y1 < 0 || y2 >= FIELD_DIMENSION) {
+        return false;
+    }
+
+    for (var x = x1; x <= x2; ++x) {
+        for (var y = y1; y <= y2; ++y) {
+            var cell = MAP[x][y];
+
+            if (cell !== 0) {
+                return;
+            }
+        }
+    }
+
+    return true;
+}
+
+function updatePosition(object) {
     switch (object.direction) {
         case 0:
-            toPos[0] += object.speed;
+            object.position[1] -= object.speed;
             break;
 
         case 1:
-            toPos[1] += object.speed;
+            object.position[0] += object.speed;
             break;
 
         case 2:
-            toPos[0] -= object.speed;
+            object.position[1] += object.speed;
             break;
 
         case 3:
-            toPos[1] -= object.speed;
+            object.position[0] -= object.speed;
             break;
     }
+}
 
-    return toPos;
+function jointFilter(obj) {
+    return obj.joint;
 }
 
 /*
  * Send updates to users.
  */
 setInterval(function() {
-    var players = PLAYERS.map(function(player) {
-         return player.position;
+    var players = PLAYERS.filter(jointFilter).map(function(player) {
+         return {
+             id: player.id,
+             position: player.position,
+             direction: player.direction
+         };
     });
 
     var bullets = BULLETS.map(function(bullet) {
@@ -182,7 +306,6 @@ setInterval(function() {
     var data = {
         event: 'updateMapState',
         data: {
-            map: MAP,
             players: players,
             bullets: bullets
         }
@@ -191,6 +314,26 @@ setInterval(function() {
     broadcast(data);
 
 }, SOCKET_SEND_INTERVAL);
+
+/**
+ * Отправить сообщение одному пользователю.
+ * @param {PLAYER} player
+ * @param {Object} data
+ */
+function send(player, data) {
+    var json;
+
+    try {
+        json = JSON.stringify(data);
+    } catch(e) {
+        console.log('BROADCAST stringify failed', e);
+        return;
+    }
+
+    if (player.socket) {
+        player.socket.send(json);
+    }
+}
 
 /**
  * Отправляет сообщение всем игрокам.
@@ -212,11 +355,14 @@ function broadcastExcept(data, except) {
         json = JSON.stringify(data);
     } catch(e) {
         console.log('BROADCAST stringify failed', e);
+        return;
     }
 
     PLAYERS.forEach(function(player) {
-        if (player !== except) {
-            player.socket.send(json);
+        if (player.joint && player !== except) {
+            if (player.socket) {
+                player.socket.send(json);
+            }
         }
     });
 }
